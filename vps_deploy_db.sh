5d5673f80f2918357bef08cfcd1cd0e50e805ca9
waylon wang <waylonwang@users.noreply.github.com>
2019-03-04 00:39:54 +0800

    Update vps_deploy_db.sh

--- vps_deploy_db.sh
+++ vps_deploy_db.sh
@@ -2,8 +2,6 @@
 
 [ -z $REF_CONFLICT_FLAG ] && source <(curl -s https://raw.githubusercontent.com/waylonwang/sh-scripts/master/lib/public_const.sh)
 
-export DB_HOST="db.cloudvalley.pw"
-
 function init_config_folder()
 {
   [ ! -d "docker" ] && mkdir docker

663247c4879d23ebd6082f407fea7181c8b7a5fa
waylon wang <waylonwang@users.noreply.github.com>
2019-04-28 17:48:26 +0800

    Update vps_deploy_db.sh

--- vps_deploy_db.sh
+++ vps_deploy_db.sh
@@ -1,5 +1,13 @@
 # !/bin/bash
-
+#*************************************************************************************
+# 本脚本实现了在VPS中部署数据库环境的脚本功能
+# 本脚本使用方法:
+#  curl -L https://raw.githubusercontent.com/waylonwang/sh-scripts/master/vps_deploy_db.sh -o vps_deploy_db.sh && chmod +x vps_deploy_db.sh && ./vps_deploy_db.sh
+#  或
+#  wget --no-check-certificate https://raw.githubusercontent.com/waylonwang/sh-scripts/master/vps_deploy_db.sh && chmod +x vps_deploy_db.sh && ./vps_deploy_db.sh
+# 
+# 作者:waylon@waylon.wang
+#*************************************************************************************
 [ -z $REF_CONFLICT_FLAG ] && source <(curl -s https://raw.githubusercontent.com/waylonwang/sh-scripts/master/lib/public_const.sh)
 
 function init_config_folder()
@@ -15,9 +23,28 @@ function init_config_folder()
   source <(curl -s https://raw.githubusercontent.com/waylonwang/sh-scripts/master/lib/check_db_volumes.sh)
   check_mysql_volumes -c -p
   check_phpmyadmin_volumes -c -p
-
-  [ ! -e "./nginx/conf/portainer.conf" ] && add_nginx_conf -d "./nginx" -n "portainer" -p
-  [ ! -e "./nginx/conf/mysql.conf" ] && add_nginx_conf -d "./nginx" -n "mysql" -p
+  
+  local answer1,answer2
+  read -p $"Add nginx configuration file of portainer?[Y/n]: " answer1
+  
+  case ${answer1:0:1} in
+    n|N )
+        echo -e "${CLR_FG_GR}[OK]${CLR_NO} skip add configuration file."
+    ;;
+    * )
+        [ ! -e "./nginx/conf/portainer.conf" ] && add_nginx_conf -d "./nginx" -n "portainer" -p
+    ;;
+  esac
+  read -p $"Add nginx configuration file of mysql?[Y/n]: " answer1
+  
+  case ${answer1:0:1} in
+    n|N )
+        echo -e "${CLR_FG_GR}[OK]${CLR_NO} skip add configuration file."
+    ;;
+    * )
+        [ ! -e "./nginx/conf/mysql.conf" ] && add_nginx_conf -d "./nginx" -n "mysql" -p
+    ;;
+  esac
 }
 
 function download_compose_file()
@@ -47,7 +74,7 @@ function main(){
   source <(curl -s https://raw.githubusercontent.com/waylonwang/sh-scripts/master/lib/check_docker_env.sh)
   echo -e "${CLR_FG_PU}Start deploying db Service${CLR_NO}"
 
-  check_os -t "centos" -p
+  check_os -t "centos,ubuntu" -p
   if [ $? -eq 0 ]; then
     check_docker_install -a -i -p -s -v
     check_compose_install -i -p -v

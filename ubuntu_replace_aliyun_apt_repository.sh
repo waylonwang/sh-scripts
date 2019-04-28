148b0b6f7d6bcc2e275e4f9032b34f1a887c4922
waylon wang <waylonwang@users.noreply.github.com>
2019-04-28 23:53:25 +0800

    Update ubuntu_replace_aliyun_apt_repository.sh

--- ubuntu_replace_aliyun_apt_repository.sh
+++ ubuntu_replace_aliyun_apt_repository.sh
@@ -3,15 +3,19 @@
 # Licensed under the MIT License
 
 #*************************************************************************************
-# 本脚本实现了ubuntu的apt源更改为阿里云镜像的脚本功能
+# 本脚本实现了ubuntu、debian的apt源更改为阿里云镜像的脚本功能
 # 本脚本使用方法:
 #  wget --no-check-certificate https://raw.githubusercontent.com/waylonwang/sh-scripts/master/ubuntu_replace_aliyun_apt_repository.sh && chmod +x ubuntu_replace_aliyun_apt_repository.sh && ./ubuntu_replace_aliyun_apt_repository.sh
 # 
 # 作者:waylon@waylon.wang
 #*************************************************************************************
+source <(curl -s https://raw.githubusercontent.com/waylonwang/sh-scripts/master/lib/check_os_env.sh)
+#检查OS环境
+check_os -t "debian,ubuntu" -p
+
 codename=`sudo lsb_release -c | sed -n -r '1,1 s/.*Codename:\s*(\S*)/\1/p'`
 
-sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
+sudo rm -f /etc/apt/sources.list
 
 echo "deb http://mirrors.aliyun.com/ubuntu/ $codename main restricted universe multiverse" >> /etc/apt/sources.list
 echo "deb-src http://mirrors.aliyun.com/ubuntu/ $codename main restricted universe multiverse" >> /etc/apt/sources.list
